6d04364876d406b62b7f068179ed8bc5410080a8
Bernardo Aguayo Ortega <bernardo.aguayo.ortega@gmail.com>
2021-01-12 10:54:35 -0600

    [add] add virtual enviroment

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ def hello_world():
 
 @app.route('/super_simple')
 def super_simple():
-    return 'Hello from the planetary API. Some planet'
+    return 'Hello from the planetary API. Some planet, New some planet'
 
 
 if __name__ == '__main__':
